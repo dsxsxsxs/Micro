d881a80b4350dc7ea28d1306dca496145076b0b5
dsxsxsxs <dsxsxsxs@gmail.com>
2023-10-31 20:47:37 +0900

    samples

--- Sources/MicroClient/main.swift
+++ Sources/MicroClient/main.swift
@@ -6,3 +6,44 @@ let b = 25
 let (result, code) = #stringify(a + b)
 
 print("The value \(result) was produced by the code \"\(code)\"")
+
+enum Store {
+    static func save(_ value: Any) {}
+}
+
+// given
+struct User {
+    let id: Int
+    let name: String
+}
+
+// @WithStoreModel
+// or
+// #StaticStoreModel {
+//    struct User {
+//        let id: Int
+//        let name: String
+//    }
+// }
+struct UserModel: Codable {
+    let id: Int
+    let name: String
+
+    func save() {
+        Store.save(id)
+        Store.save(name)
+    }
+}
+// @StoreModel
+extension User: Codable {
+    func save() {
+        Store.save(id)
+        Store.save(name)
+    }
+}
+
+
+
+// #emptyStoreModel {
+//    UserModel(id: 0, name: "")
+//}
